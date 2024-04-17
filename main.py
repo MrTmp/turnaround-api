542e6a6038d17302551dcb09a00222fe9e18e038
MrTmp <134160202+MrTmp@users.noreply.github.com>
2024-04-17 01:29:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,6 @@ import json
 import time
 import os
 
-def setup_solver(): #please body dont sue me
-    if not os.path.exists("utils"): os.mkdir("utils")
-    files = ["https://raw.githubusercontent.com/Body-Alhoha/turnaround/main/utils/solver.py", "https://raw.githubusercontent.com/Body-Alhoha/turnaround/main/utils/page.html"]
-    for file in files:
-        r = requests.get(file).text
-        with open("utils/" + file.split("/")[-1], "w") as f:
-            f.write(r)
-
 setup_solver()
 app = flask.Flask(__name__)
 from utils import solver
